754b87aed28eafe39a35b9e953afc8e381f24d78
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-05-28 22:33:20 -0400

    all

--- group-sizes.js
+++ group-sizes.js
@@ -18,6 +18,6 @@ if (remainNum == 1){
     const twoThre = ( (2*(remTwo + 1))/2);
     console.log ("There are", Math.round(gthreeMone), "and", twoThre, "groups of two.")
 } else if (remainNum == 0) {
-    console.log ("There are",groupThree, "and", "zero groups of two!")
+    console.log ("There are",groupThree, "groups of three and", "zero groups of two!")
 }
 
